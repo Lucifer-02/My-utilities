4c01a5cc88c759951a28c82fe4c7537edffd67b0
Lucifer-02 <lucifer.02.nice@gmail.com>
2023-01-17 20:07:58 +0700

    update

--- SpeakFasterWindows/speak_translated_faster_Windows.py
+++ SpeakFasterWindows/speak_translated_faster_Windows.py
@@ -1,27 +1,74 @@
-from subprocess import check_output, Popen, PIPE
+from subprocess import Popen, PIPE
 from gtts.tts import gTTS
 import pyautogui
 import pyperclip
 from keyboard import wait
-from translators import google
+from translators import translate_text
+import multiprocessing
+import os
 
 
+# configs
 speed = 2.0
+shortkey = r"F9"
 
+# kill prosses
+
+
+def kill(pid: int) -> bool:
+    try:
+        os.kill(pid, 9)
+    except OSError:
+        return False
+    else:
+        return True
+
+
+def get_text():
+    pyautogui.hotkey('ctrl', 'c')
+    return pyperclip.paste()
 
 # translate selected text and read
-def trans():
-    pyautogui.hotkey('ctrl', 'c')
-    text = pyperclip.paste()
-    translated = google(text, "en", "vi")
-    tts = gTTS(text=translated, lang='vi')
+
+
+def trans(text):
+    return translate_text(text, translator='google', to_language='vi')
+
+
+def tts_mpv(text):
+    tts = gTTS(text=text, lang='vi')
     p = Popen(['mpv', '--speed=' + str(speed), '--no-video', '-'], stdin=PIPE)
     tts.write_to_fp(p.stdin)
-    p.stdin.close()
     p.wait()
 
 
-shortkey = r"ctrl+`"
-while True:
-    wait(shortkey)
-    trans()
+def tts_ffmpeg(text):
+    tts = gTTS(text=text, lang='vi')
+    p = Popen(['ffplay', '-af', 'atempo=' + str(speed),
+              '-nodisp', '-autoexit'], stdin=PIPE)
+    tts.write_to_fp(p.stdin)
+    p.wait()
+
+
+def run(prev_pid: int):
+    if kill(prev_pid):
+        print(f"killed {prev_pid}")
+    else:
+        tts_mpv(trans(get_text()))
+        #  tts_ffmpeg(trans(get_text()))
+
+
+def main():
+    prev_pid = 0
+    while True:
+        wait(shortkey)
+        p = multiprocessing.Process(target=run, args=(prev_pid,))
+        p.start()
+        prev_pid = p.pid
+
+    #  wait(shortkey)
+    #  run(0)
+
+
+if __name__ == "__main__":
+    main()
