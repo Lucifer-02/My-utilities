c065ca4f2ca3e851620e796d5cf47f0537194a2e
Lucifer-02 <lucifer.02.nice@gmail.com>
2023-06-08 11:27:18 +0700

    modify english exercise tool

---
+++ eng_prac/count_word.py
@@ -0,0 +1,70 @@
+from termcolor import colored
+import datetime
+from subprocess import check_output
+
+
+# get content with xsel (linux) and check_output
+def get_clipboard() -> str:
+    return check_output("xsel").decode("utf-8")
+
+
+def normalize_text(text) -> str:
+    return text.lower().replace("\n", "").replace("\r", "").replace("\t", "")
+
+
+def get_char_count(text, char) -> int:
+    return text.count(char)
+
+
+def write_to_file(file_path, content, char, gess, answer):
+    with open(file_path, "a") as file:
+        file.write(
+            str(datetime.datetime.now())
+            + ";"
+            + content
+            + ";"
+            + char
+            + ";"
+            + str(gess)
+            + ";"
+            + str(answer)
+            + "\n"
+        )
+    file.close()
+
+
+def main():
+    content = normalize_text(get_clipboard())
+
+    char = input("Character to count: ")
+    gess = int(input('Guess number of "' + colored(char, "black", "on_white") + '": '))
+    answer = get_char_count(content, char)
+
+    if gess == answer:
+        print(colored("Correct!", "black", "on_green"))
+    else:
+        print(colored("Wrong!", "black", "on_red"))
+
+    print(
+        colored(char, "black", "on_white")
+        + " is: "
+        + colored(str(answer), "black", "on_magenta")
+    )
+
+    # print content with highlighted char
+    print("---------------------------------Content---------------------------------")
+    print(content.replace(char, colored(char, "black", "on_white")))
+    print("-------------------------------------------------------------------------")
+
+    # ATTENTION: change path to your needs
+    path = "/media/lucifer/DATA/My-utilities/eng_prac/data.csv"
+
+    write_to_file(path, content, char, gess, answer)
+    # print("Data written to: " + path)
+
+
+if __name__ == "__main__":
+    main()
+    # press any key to exit
+    print("Press any key to exit...")
+    input()
--- speak_translated_faster.py
+++ speak_translated_faster.py
@@ -2,33 +2,35 @@ from subprocess import Popen, check_output, PIPE
 from gtts.tts import gTTS
 import psutil
 
+
 def checkIfProcessRunning(processName):
-    '''
+    """
     Check if there is any running process that contains the given name processName.
-    '''
-    #Iterate over the all the running process
+    """
+    # Iterate over the all the running process
     for proc in psutil.process_iter():
         try:
             # Check if process name contains the given name string.
-            if processName.lower() in proc.name().lower() and proc.status() != 'zombie':
+            if processName.lower() in proc.name().lower() and proc.status() != "zombie":
                 return True
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
-    return False;
+    return False
+
 
 def handle():
     speed = 2.0
 
-    text = check_output(['xsel'])
-    translated = check_output(['crow', '-b', '-t', 'vi', text]).decode('utf-8')
-    tts = gTTS(text=translated, lang='vi')
-    p = Popen(['mpv', '--speed=' + str(speed), '--no-video', '-'], stdin=PIPE)
+    text = check_output(["xsel"])
+    translated = check_output(["crow", "-b", "-t", "vi", text]).decode("utf-8")
+    tts = gTTS(text=translated, lang="vi")
+    p = Popen(["mpv", "--speed=" + str(speed), "--no-video", "-"], stdin=PIPE)
     tts.write_to_fp(p.stdin)
     p.stdin.close()
     p.wait()
 
 
-if(checkIfProcessRunning('mpv') == True):
-    Popen(['killall','mpv'])
+if checkIfProcessRunning("mpv") == True:
+    Popen(["killall", "mpv"])
 else:
     handle()
