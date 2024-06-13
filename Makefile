e4e9e93699c4897717b30b7fea666773fbfd742a
HOANGNLV.HO <HOANGNLV.HO@VIETCOMBANK.COM.VN>
2024-06-13 07:54:06 +0700

    add outline tool

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 run:
-	python3 ./edit_translated.py
+	# python3 ./edit_translated.py
 	# python3 ./test.py
+	python3 ./add_outline.py && open ./output.pdf
+
 
---
+++ add_outline.py
@@ -0,0 +1,59 @@
+import yaml
+
+import pikepdf
+from pikepdf import OutlineItem
+
+
+def create_item(item: dict[str, str]) -> OutlineItem:
+    return OutlineItem(title=item["title"], destination=int(item["page"]) - 1)
+
+
+def create_toc(toc: dict) -> OutlineItem:
+    return create_item(toc)
+
+
+def create_part(part: dict) -> OutlineItem:
+    part_item = create_item(part)
+    for chapter in part["chapters"]:
+        try:
+            part_item.children.append(create_chapter(chapter))
+        except KeyError:
+            # review part
+            part_item.children.append(create_item(chapter))
+
+    return part_item
+
+
+def create_chapter(chapter: dict) -> OutlineItem:
+    chapter_item = create_item(chapter)
+    for section in chapter["sections"]:
+        chapter_item.children.append(create_item(section))
+    return chapter_item
+
+
+def create_appendix(appendixes: list[dict]) -> list[OutlineItem]:
+    appendix_items = []
+    for appendix in appendixes:
+        appendix_item = create_item(appendix)
+        appendix_items.append(appendix_item)
+
+    return appendix_items
+
+
+if __name__ == "__main__":
+    pdf = pikepdf.open("./book.pdf")
+    with open("./outline.yaml", "r") as f:
+        outline_input = yaml.safe_load(f)
+
+    with pdf.open_outline() as outline:
+        toc = create_toc(outline_input["toc"])
+        outline_parts: list[OutlineItem] = [
+            create_part(part) for part in outline_input["parts"]
+        ]
+        appendixies = create_appendix(outline_input["appendixes"])
+
+        # set the outline
+        for item in [toc] + outline_parts + appendixies:
+            outline.root.append(item)
+
+    pdf.save("output.pdf")
